d92e74cd9c85e21445f48776ad1e9b819ca3f767
Jayce Azua <jayce.azua@gmail.com>
2020-01-17 16:34:50 -0800

    Delete avlnode.py

--- Code/avlnode.py
+++
@@ -1,7 +0,0 @@
-class AVLNode:
-    def __init__(self, value):
-        self.value = value
-        self.left_child = None
-        self.right_child = None
-        self.parent = None
-        self.height = 1
